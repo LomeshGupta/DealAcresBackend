cdafa3fbddbda0e96129e199ff7b5a386ee2e70f
LomeshGupta <guptabittu06@gmail.com>
2024-03-03 19:52:18 +0530

    propert

--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -5,8 +5,8 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post("/create", propertyController.createProperty);
-router.get("/getall", propertyController.getAllProperties);
+router.post("/", propertyController.createProperty);
+router.get("/", propertyController.getAllProperties);
 router.get("/:id", propertyController.getPropertyById);
 router.put("/:id", propertyController.updatePropertyById);
 router.delete("/:id", propertyController.deletePropertyById);
