407d081c8ca930eae0100f305ca91398bd414fa6
LomeshGupta <guptabittu06@gmail.com>
2024-04-23 22:43:14 +0530

    check

--- index.js
+++ index.js
@@ -19,15 +19,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: "*",
-};
-
-//middlewares
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+// Middleware setup
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(cors());
 
 //routesmiddleware
