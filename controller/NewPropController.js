6e8df5283d8c85f401b9ce9c42eda26529e56a5d
LomeshGupta <guptabittu06@gmail.com>
2024-06-27 18:12:04 +0530

    numbersmap

--- controller/NewPropController.js
+++ controller/NewPropController.js
@@ -9,7 +9,11 @@ const parseNestedStructure = (data) => {
   if (data.sidePics) data.sidePics = data.sidePics.split(",");
   if (data.bathroomInfo) data.bathroomInfo = data.bathroomInfo.split(",");
   if (data.bedroomInfo) data.bedroomInfo = data.bedroomInfo.split(",");
-  if (data.Amenities) data.Amenities = data.Amenities.split(",");
+  if (data.Amenities) {
+    data.Amenities = data.Amenities.split(",").map((item) =>
+      Number(item.trim())
+    );
+  }
 
   // Parse JSON structures
   if (data.roomInfo) data.roomInfo = JSON.parse(data.roomInfo);
