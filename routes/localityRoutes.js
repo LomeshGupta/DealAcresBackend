d76de4ea5a1f722105416012942ffbc9fe201c88
Lomesh Gupta <guptabittu06@gmail.com>
2024-08-26 22:37:51 +0530

    chnges

--- routes/localityRoutes.js
+++ routes/localityRoutes.js
@@ -6,11 +6,15 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post("/upload", upload.single("file"), localityController.upload);
+router.post(
+  "/upload",
+  upload.single("file"),
+  localityController.uploadLocalities
+);
 
 // Other routes
 router.post("/", localityController.createLocality);
-router.get("/", localityController.getAll);
+router.get("/", localityController.getAllLocalities);
 router.get("/:id", localityController.getLocalityById);
 router.put("/:id", localityController.updateLocality);
 router.delete("/:id", localityController.deleteLocality);
